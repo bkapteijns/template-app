b702bda48172e84e798762430cf971be4508f62f
bkapteijns <bram_kapteijns@hotmail.com>
2020-07-18 17:36:59 +0200

    navbar

--- client/index.js
+++ client/index.js
@@ -1,4 +1,17 @@
 import React from "react";
-import { render } from "react-dom";
+import ReactDOM from "react-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-render(<div>App</div>, document.getElementById("root"));
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+ReactDOM.render(
+  <Auth0Provider
+    domain="dev-g9blhnj8.eu.auth0.com"
+    clientId="PuxEFXXtq4RzYZkHbqzi16g1akqhRQBV"
+    redirectUri="http://localhost:3000/profile"
+  >
+    <App />
+  </Auth0Provider>,
+  document.getElementById("root")
+);
